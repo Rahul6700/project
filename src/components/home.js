c7d5aacf4817bdbfad030fc0b514678bc4810556
Rahul Senthil Kumar <rahulsk4087@gmail.com>
2024-11-08 06:53:29 +0530

    room code

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,19 @@
+import React, { useState } from 'react';
+import { customAlphabet } from 'nanoid';
+
 export default function Home() {
+    const [roomCode, setRoomCode] = useState(''); //the room code that is created
+    const [inputCode, setInputCode] = useState(''); //the user input in textarea
+
+    const createParty = () => {
+        const generateRoomId = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 6);
+        setRoomCode(generateRoomId());
+    };
+
+    const handleCode = () => {
+        // logic to reroute if the code is correct else error handling
+    };
+
     return (
         <div
             style={{
@@ -10,10 +25,10 @@ export default function Home() {
                 textAlign: "center",
                 fontFamily: "system-ui, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
                 boxShadow: "5px 5px 8px rgb(0,0,100)",
-                borderRadius: "15px"
+                borderRadius: "15px",
             }}
         >
-            <button
+            <button onClick={createParty}
                 style={{
                     backgroundColor: "#333",
                     color: "#fff",
@@ -22,15 +37,18 @@ export default function Home() {
                     borderRadius: "4px",
                     cursor: "pointer",
                     marginBottom: "5px",
-                    fontSize: "24px"
+                    fontSize: "24px",
                 }}
             >
                 Create Party
             </button>
+            {roomCode && <h2>Room Code: {roomCode}</h2>}
             <h1>OR</h1>
             <div style={{ display: "flex", alignItems: "center", gap: "10px", justifyContent: "center" }}>
                 <textarea
                     placeholder="Enter code"
+                    value={inputCode}
+                    onChange={(e) => setInputCode(e.target.value)}
                     style={{
                         padding: "10px",
                         borderRadius: "4px",
@@ -40,10 +58,10 @@ export default function Home() {
                         resize: "none",
                         width: "60%",
                         height: "24px",
-                        fontSize: "16px"
+                        fontSize: "16px",
                     }}
                 ></textarea>
-                <button
+                <button onClick={handleCode}
                     style={{
                         backgroundColor: "#333",
                         color: "#fff",
@@ -51,7 +69,7 @@ export default function Home() {
                         padding: "10px 30px",
                         borderRadius: "4px",
                         cursor: "pointer",
-                        fontSize: "20px"
+                        fontSize: "20px",
                     }}
                 >
                     Submit
